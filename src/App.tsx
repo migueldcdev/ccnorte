2a5f7d49bad6768ad01a90f14e060cebedcdf731
migueldcdev <migueldc.dev@protonmail.com>
2023-04-24 13:04:45 +0200

    add ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <div className="flex flex-col justify center mt-20">
+        <div className="w-2/3 md:w-1/2 mx-auto text-center ">
+         <p className="text-2xl font-bold">Scan your ID</p>
+         <p>Take a picture. It may take time to validate your personal information.</p> 
+        </div>
+        <div className="w-2/3 md:w-1/2 flex justify-center mx-auto mt-20 shadow rounded py-12 px-2">
+            <button className="bg-indigo-800 text-white font-bold py-2 px-4 rounded-full">TAKE PICTURE</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+
+function NavBar() {
+    return (
+
+        <div className="p-5 shadow">
+            <p className="text-xl text-indigo-800 font-bold italic">BankClient</p>
+        </div>
+
+    )
+}
+
+export default NavBar
