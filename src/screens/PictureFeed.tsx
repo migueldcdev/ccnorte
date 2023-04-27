d0719c7d0252928a3eccdbff769f513c51000845
migueldcdev <migueldc.dev@protonmail.com>
2023-04-27 13:30:27 +0200

    fit camera inside container 0.6

--- src/screens/PictureFeed.tsx
+++ src/screens/PictureFeed.tsx
@@ -78,7 +78,7 @@ function PictureFeed() {
             <div className="w-80 h-52 mx-auto mt-20 shadow rounded border border-1 border-white">
                 <Webcam
                     audio={false}
-                    className="aspect-video"
+                    className="w-full"
                     screenshotFormat="image/jpeg" 
                     videoConstraints={videoConstraints}                                       
                 />
