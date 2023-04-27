03b0a15cfc5f799e25af00070f864e8bf9a8735a
migueldcdev <migueldc.dev@protonmail.com>
2023-04-27 17:14:39 +0200

    front image render 2.7

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -33,7 +33,7 @@ function Home() {
                             <img src={location.state.image} alt="Uploaded" className="w-full absolute" />
                             <button type="button" className="bg-indigo-900 text-white font-bold px-5 py-3 rounded-full absolute my-20" onClick={() => handleClick()}>RETAKE PICTURE</button>                            
                         </div>
-                        <button className="text-white text-sm bg-red-600 px-2 py-2 top-64 mt-5 right-10 rounded absolute index-100"><i className="fa fa-xmark"></i> REJECTED</button>
+                        <button className="text-white text-sm bg-red-600 px-2 py-2 top-64 mt-5 right-10 rounded absolute index-100">&#x2713; REJECTED</button>
                     </div>
                 }
                 {location.state !== null && location.state.validated === true &&
