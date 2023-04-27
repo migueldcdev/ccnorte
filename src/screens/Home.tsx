15a0ca3f5b7c56a987477f3f987e8f5bc54e2b26
migueldcdev <migueldc.dev@protonmail.com>
2023-04-27 15:08:03 +0200

    camera logic 0.8

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -23,6 +23,7 @@ function Home() {
         <div>
 
             <NavBar />
+            {location.state == null &&
             <div className="flex flex-col justify-center mt-10">
                 <div className="w-2/3 md:w-1/2 mx-auto text-center ">
                     <p className="text-2xl font-bold">Scan your ID</p>
@@ -32,12 +33,16 @@ function Home() {
                     <button type="button" className="bg-indigo-900 text-white font-bold px-5 py-3 rounded-full absolute my-20" onClick={() => handleClick()}>TAKE PICTURE</button>
                 </div>
             </div>
-
+            }
             <div className="flex justify-center">
-                {location.state != null &&
-                    <div className="mt-20 flex flex-col justify-center text-center mb-20">
-                        <img src={location.state.image} alt="Uploaded image" width={250}/>
-                        <p className="text-lime-600 text-2xl font-bold">Success!</p>
+                {location.state != null && location.state.validated == false &&
+                   <div className="w-80 h-52 flex justify-center mx-auto mt-20 shadow rounded relative border border-1 border-rose-500">
+                        <img src={location.state.image} alt="Uploaded image" width={250}/>                        
+                    </div>
+                }
+                {location.state != null && location.state.validated == true &&
+                   <div className="w-80 h-52 flex justify-center mx-auto mt-20 shadow rounded relative border border-1 border-lime-500">
+                        <img src={location.state.image} alt="Uploaded image" width={250}/>                        
                     </div>
                 }
 
