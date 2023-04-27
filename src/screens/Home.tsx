c19634593697b53a080eeb98c7ec21888db43981
migueldcdev <migueldc.dev@protonmail.com>
2023-04-27 16:44:19 +0200

    front image render 2.2

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -41,10 +41,8 @@ function Home() {
                     <div className="w-80 h-52 flex justify-center mx-auto mt-20 shadow rounded border border-2 border-red-600 overflow-y-hidden relative">
                         <img src={location.state.image} alt="Uploaded image" className="w-full absolute" />
                         <button type="button" className="bg-indigo-900 text-white font-bold px-5 py-3 rounded-full absolute my-20" onClick={() => handleClick()}>RETAKE PICTURE</button>
-                        <button className="text-white text-sm bg-red-600 px-1 py-1 top-80 mt-5 right-10 rounded absolute index-10"><i className="fa fa-car"></i> REJECTED</button>
+                        <button className="text-white text-sm bg-red-600 px-1 py-1 top-64 mt-5 right-10 rounded absolute index-20"><i className="fa fa-car"></i> REJECTED</button>
                     </div>
-
-
                 }
                 {location.state != null && location.state.validated == true &&
                     <div className="w-80 h-52 mx-auto mt-20 shadow rounded border border-2 border-lime-500 overflow-y-hidden relative">
