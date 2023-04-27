4facde45b16b8195195f17e06e722b69417ac72d
migueldcdev <migueldc.dev@protonmail.com>
2023-04-27 13:09:00 +0200

    fit camera inside container 0.3

--- src/screens/PictureFeed.tsx
+++ src/screens/PictureFeed.tsx
@@ -72,7 +72,7 @@ function PictureFeed() {
             <div className="w-80 h-52 mx-auto mt-20 shadow rounded border border-1 border-white">
                 <Webcam
                     audio={false}
-                    className="w-80 h-52"
+                    className="w-full h-full"
                     screenshotFormat="image/jpeg"
                                         
                 />
