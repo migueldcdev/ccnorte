9c5af1eaaf3631bd9c3a0b56df42e5e33125fe0f
migueldcdev <migueldc.dev@protonmail.com>
2023-04-27 15:12:42 +0200

    front image render

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -23,26 +23,28 @@ function Home() {
         <div>
 
             <NavBar />
-            {location.state == null &&
             <div className="flex flex-col justify-center mt-10">
                 <div className="w-2/3 md:w-1/2 mx-auto text-center ">
                     <p className="text-2xl font-bold">Scan your ID</p>
                     <p className="mt-10">Take a picture. It may take time to validate your personal information.</p>
                 </div>
-                <div className="w-80 h-52 flex justify-center mx-auto mt-20 shadow rounded relative">
-                    <button type="button" className="bg-indigo-900 text-white font-bold px-5 py-3 rounded-full absolute my-20" onClick={() => handleClick()}>TAKE PICTURE</button>
-                </div>
+                {location.state == null &&
+
+                    <div className="w-80 h-52 flex justify-center mx-auto mt-20 shadow rounded relative">
+                        <button type="button" className="bg-indigo-900 text-white font-bold px-5 py-3 rounded-full absolute my-20" onClick={() => handleClick()}>TAKE PICTURE</button>
+                    </div>
+
+                }
             </div>
-            }
             <div className="flex justify-center">
                 {location.state != null && location.state.validated == false &&
-                   <div className="w-80 h-52 flex justify-center mx-auto mt-20 shadow rounded relative border border-1 border-rose-500">
-                        <img src={location.state.image} alt="Uploaded image" width={250}/>                        
+                    <div className="w-80 h-52 flex justify-center mx-auto mt-20 shadow rounded relative border border-1 border-rose-500 overflow-y-hidden">
+                        <img src={location.state.image} alt="Uploaded image" width={250} />
                     </div>
                 }
                 {location.state != null && location.state.validated == true &&
-                   <div className="w-80 h-52 flex justify-center mx-auto mt-20 shadow rounded relative border border-1 border-lime-500">
-                        <img src={location.state.image} alt="Uploaded image" width={250}/>                        
+                    <div className="w-80 h-52 flex justify-center mx-auto mt-20 shadow rounded relative border border-1 border-lime-500 overflow-y-hidden">
+                        <img src={location.state.image} alt="Uploaded image" width={250} />
                     </div>
                 }
 
